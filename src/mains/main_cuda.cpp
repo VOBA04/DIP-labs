6e170a6f3dbc3adaa8b003189cf56eaeb5ba4b21
ULADZIMIR BALASHENKA <vova.bolashenko@gmail.com>
2025-09-22 22:44:36 +0300

    решение

--- src/mains/main_cuda.cpp
+++ src/mains/main_cuda.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/core/hal/interface.h>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -56,14 +57,52 @@ int main(int argc, char *argv[]) {
       {hue8.ToCvMat(CV_8UC1), sat8.ToCvMat(CV_8UC1), int8img.ToCvMat(CV_8UC1)},
       "HSI Channels (normalized)");
 
-  std::vector<CudaImage<uint8_t>> bgr_from_hsi(3);
-  CudaHsiToBgr(cuda_hsi_channels, bgr_from_hsi);
-  cv::Mat b_mat = bgr_from_hsi[0].ToCvMat(CV_8UC1);
-  cv::Mat g_mat = bgr_from_hsi[1].ToCvMat(CV_8UC1);
-  cv::Mat r_mat = bgr_from_hsi[2].ToCvMat(CV_8UC1);
-  cv::Mat bgr_recon;
-  cv::merge(std::vector<cv::Mat>{b_mat, g_mat, r_mat}, bgr_recon);
-  ShowImages({bgr_recon}, "BGR reconstructed from HSI");
+  CudaImage<uint8_t> sat_mask;
+  CudaImage<uint8_t> sat_mask2;
+  CudaImage<uint8_t> sat_mask3;
+  CudaInRange(sat8, 150, 255, sat_mask);
+  const size_t KERNEL_SIZE_1 = 5;
+  CudaImage<uint8_t> morph_kernel(KERNEL_SIZE_1, KERNEL_SIZE_1, 1);
+  CudaDilation(sat_mask, morph_kernel, sat_mask2);
+  CudaErosion(sat_mask2, morph_kernel, sat_mask3);
+  CudaImage<uint8_t> sat_mask4;
+  CudaImage<uint8_t> sat_mask5;
+  CudaImage<uint8_t> sat_mask6;
+  CudaImage<uint8_t> sat_mask7;
+  const size_t KERNEL_SIZE_2 = 27;
+  CudaImage<uint8_t> morph_kernel2(KERNEL_SIZE_2, KERNEL_SIZE_2, 1);
+  CudaErosion(sat_mask3, morph_kernel2, sat_mask4);
+  CudaErosion(sat_mask4, morph_kernel2, sat_mask5);
+  CudaDilation(sat_mask5, morph_kernel2, sat_mask6);
+  CudaDilation(sat_mask6, morph_kernel2, sat_mask7);
+  ShowImages({sat_mask.ToCvMat(CV_8UC1), sat_mask2.ToCvMat(CV_8UC1),
+              sat_mask3.ToCvMat(CV_8UC1), sat_mask4.ToCvMat(CV_8UC1),
+              sat_mask5.ToCvMat(CV_8UC1), sat_mask6.ToCvMat(CV_8UC1),
+              sat_mask7.ToCvMat(CV_8UC1)},
+             "Masks", 2);
+
+  std::vector<CudaImage<uint8_t>> cuda_figure_bgrs(3);
+  for (int i = 0; i < 3; i++) {
+    CudaBitwiseAnd(cuda_bgr_channels[i], sat_mask7, cuda_figure_bgrs[i]);
+  }
+  ShowImages({cuda_figure_bgrs[0].ToCvMat(CV_8UC1),
+              cuda_figure_bgrs[1].ToCvMat(CV_8UC1),
+              cuda_figure_bgrs[2].ToCvMat(CV_8UC1)},
+             "Masked BGR");
+
+  cv::Mat masked_image;
+  cv::merge(std::vector<cv::Mat>{cuda_figure_bgrs[0].ToCvMat(CV_8UC1),
+                                 cuda_figure_bgrs[1].ToCvMat(CV_8UC1),
+                                 cuda_figure_bgrs[2].ToCvMat(CV_8UC1)},
+            masked_image);
+  ShowImages({masked_image}, "Figures");
+
+  CudaImage<uint8_t> gray_image;
+  CudaBgrToGray(cuda_figure_bgrs, gray_image);
+  CudaImage<uint8_t> sobel_image;
+  CudaSobel(gray_image, sobel_image);
+  ShowImages({gray_image.ToCvMat(CV_8UC1), sobel_image.ToCvMat(CV_8UC1)},
+             "Gray and Sobel");
 
   int key;
   do {
